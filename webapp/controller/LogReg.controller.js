a5527ae0c429f8e1e9631bb1de7393fbe7591a35
krotek20 <radu_vintila20@yahoo.com>
2021-08-09 19:30:58 +0300

    Forgot password functionality

--- webapp/controller/LogReg.controller.js
+++ webapp/controller/LogReg.controller.js
@@ -1,48 +1,47 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
- ], function (Controller, MessageToast) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.LogReg", {
-      onInit: function ()
-      {
-        var oRouter = this.getOwnerComponent().getRouter();
-        oRouter.getRoute("overview").attachPatternMatched(this._onObjectMatched, this);
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  "use strict";
+  return Controller.extend("sap.ui.demo.walkthrough.controller.LogReg", {
+    onInit: function () {
+      var oRouter = this.getOwnerComponent().getRouter();
+      oRouter.getRoute("overview").attachPatternMatched(this._onObjectMatched, this);
 
-      },
-      
-      _onObjectMatched: function (oEvent) {
-       
-      },
-      onLogIn : function (oEvent) {
+    },
 
-          var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+    _onObjectMatched: function (oEvent) {
+
+    },
+    onLogIn: function (oEvent) {
+
+      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 
       var oModel = this.getOwnerComponent().getModel();
-			var sUsername = this.getView().byId("UsernameField").getValue();
-			var sPassword = this.getView().byId("PasswordField").getValue();
+      var sUsername = this.getView().byId("UsernameField").getValue();
+      var sPassword = this.getView().byId("PasswordField").getValue();
 
-			oModel.read("/UsersSet(Username='"+sUsername+"',Password='"+sPassword+"')", {
-                success: function(oRetrievedResult) { 
-                 
-                  oRouter.navTo("main", {
-                    Username: sUsername
-                  }); 
-                  }.bind(this),
-                  error: function(oError) { 
-                    
-					            sap.m.MessageToast.show("Login failed")
-                }
-              });
-       },
-       onRegister : function (oEvent) {
-         var oRouter = this.getOwnerComponent().getRouter();
-         oRouter.navTo("register"); 
-       },
+      oModel.read("/UsersSet(Username='" + sUsername + "',Password='" + sPassword + "')", {
+        success: function (oRetrievedResult) {
 
-       onForgotPass : function (oEvent) {
-         var oRouter = this.getOwnerComponent().getRouter();
-         oRouter.navTo("forgotpass"); 
-       }
-    });
- });
+          oRouter.navTo("main", {
+            Username: sUsername
+          });
+        }.bind(this),
+        error: function (oError) {
+
+          sap.m.MessageToast.show("Login failed")
+        }
+      });
+    },
+    onRegister: function (oEvent) {
+      var oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("register");
+    },
+
+    onForgotPass: function (oEvent) {
+      var oRouter = this.getOwnerComponent().getRouter();
+      oRouter.navTo("forgotpass");
+    }
+  });
+});
