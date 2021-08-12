43c4fb74bc49743a37ffb256014ae90ba698c9f7
RomontiBogdan <romontibogdan@gmail.com>
2021-08-12 11:48:09 +0300

    Filter-Peg-completed

--- webapp/controller/DisplayPEGList.controller.js
+++ webapp/controller/DisplayPEGList.controller.js
@@ -19,9 +19,25 @@ sap.ui.define([
                 Manager : "Manager1",
                 Status : true
                
-            },{
+            },
+			{
 				Project  : "Project2",
                 Manager : "Manager2",
+                Status : false
+            },
+			{
+				Project  : "ProjectSuper2",
+                Manager : "Manager4",
+                Status : false
+            },
+			{
+				Project  : "ProjectSuper3",
+                Manager : "Manager3",
+                Status : true
+            },
+			{
+				Project  : "Project4",
+                Manager : "Manager5",
                 Status : false
             }]);
 
@@ -52,17 +68,49 @@ sap.ui.define([
 		},
 
 		onFilterProject : function (oEvent) {
-
 			var aFilter = [];
 			var sQuery = oEvent.getParameter("query");
-			if (sQuery) {
-				aFilter.push(new Filter("Project", FilterOperator.Contains, sQuery));
+			
+			if(this.getView().byId("PendingSwitch").getState())
+			{
+				aFilter.push(new Filter({
+					filters: [
+					new Filter("Status", FilterOperator.EQ, false),
+					new Filter("Project", FilterOperator.Contains, sQuery),
+					],
+					and: true,
+				}));
 			}
+			else
+				aFilter.push(new Filter("Project", FilterOperator.Contains, sQuery));
 
 			var oList = this.byId("pegTable");
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(aFilter);
-		}
+		},
+
+		onPendingFilter: function(oEvent)
+        {
+			var aFilter = [];
+			var sInput = this.getView().byId("sfInput").getValue()
+            if (oEvent.getParameter("state")==true)
+            {
+				aFilter.push(new Filter({
+					filters: [
+					new Filter("Status", FilterOperator.EQ, false),
+					new Filter("Project", FilterOperator.Contains, sInput),
+					],
+					and: true,
+				}));
+            }
+            else
+            {
+				aFilter.push(new Filter("Project", FilterOperator.Contains, sInput));
+			}
+			var oList = this.byId("pegTable");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+        }
 
 	});
 });
