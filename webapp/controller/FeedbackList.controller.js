a3b38b95a4f7f52a8bc4baf263324c3d8e0519ee
RomontiBogdan <romontibogdan@gmail.com>
2021-08-14 16:20:03 +0300

    Details Done

--- webapp/controller/FeedbackList.controller.js
+++ webapp/controller/FeedbackList.controller.js
@@ -45,7 +45,7 @@ sap.ui.define([
 				window.history.go(-1);
 			} else {
 				var oRouter = this.getOwnerComponent().getRouter();
-				oRouter.navTo("overview", {}, true);
+				oRouter.navTo("main", {}, true);
 			}
 		},
 
