33e5175040bfae71681dde54555f62aded7aa677
RomontiBogdan <romontibogdan@gmail.com>
2021-08-16 11:58:31 +0300

    Formatter_Done

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -9,5 +9,31 @@ sap.ui.define([], function () {
                 return "No";
             }
 		},
+
+		feedbackRating: function (sRating) {
+			switch (sRating) {
+				case "1":
+					return "Disappointing";
+				case "2":
+					return "Mediocre";
+				case "3":
+					return "Average";
+				case "4":
+					return "Good";
+				case "5":
+					return "Excellent";
+			}
+		},
+
+		feedbackCategory: function (sCategory) {
+			switch (sCategory) {
+				case "0":
+					return "Technical";
+				case "1":
+					return "Soft";
+				case "2":
+					return "Other";
+			}
+		},
 	};
 });
