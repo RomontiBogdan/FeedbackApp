19e98bdf0d5f5d1401f2ee9c32a359ece6941d05
RomontiBogdan <romontibogdan@gmail.com>
2021-08-17 15:30:31 +0300

    Revert "Merge pull request #36 from RomontiBogdan/reviewpeg"
    This reverts commit 5fe2c6bedc58422b3f8a79e39a9f9751aa543f8e, reversing
    changes made to 360ab838091a83c9e3cbbec4e363bd592e89107c.

--- webapp/controller/ManagerFeedback.controller.js
+++ webapp/controller/ManagerFeedback.controller.js
@@ -8,9 +8,6 @@ sap.ui.define([
 		onInit: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("managerFeedback").attachPatternMatched(this._onObjectMatched, this);
-
-
-
 		},
 
 		_onObjectMatched: function (oEvent) {
