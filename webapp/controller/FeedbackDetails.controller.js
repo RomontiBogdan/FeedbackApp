5a2926a6c2fc023265743e7ed83325ec5e8ea924
RomontiBogdan <romontibogdan@gmail.com>
2021-08-18 16:39:35 +0300

    Working export

--- webapp/controller/FeedbackDetails.controller.js
+++ webapp/controller/FeedbackDetails.controller.js
@@ -1,15 +1,23 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
     "sap/ui/core/routing/History",
-	"../model/formatter"
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/export/library",
+	"sap/ui/export/Spreadsheet",
+	"sap/m/MessageToast"
 	
-], function (Controller, History, formatter) {
+], function (Controller, History, JSONModel, formatter, exportLibrary, Spreadsheet, MessageToast) {
 	"use strict";
+	var EdmType = exportLibrary.EdmType;
 	return Controller.extend("sap.ui.demo.walkthrough.controller.FeedbackDetails", {
 		formatter: formatter,
 		onInit: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("feedbackdetails").attachPatternMatched(this._onObjectMatched, this);
+
+			
+			//var projectID = this.getView().getProperty("ProjectID");
 		},
 
         onNavBack: function () {
@@ -30,6 +38,92 @@ sap.ui.define([
 			this.getView().bindElement({
 				path: "/Feedback360Set(" + sFeedbackID + ")"
 		    });
+		},
+
+		createColumnConfig: function() {
+			return [
+				{
+					label: 'Descr',
+					property: 'Descr',
+					scale: 0
+				},
+				{
+					label: 'Value',
+					property: 'Value',
+					width: '25'
+				}
+				];
+		},
+
+		onExport: function(oEvent) {
+			var aCols, aProducts, oSettings, oSheet;
+
+			var projectID = oEvent.getSource().getBindingContext().getObject().ProjectId;
+
+			var oViewModel = new JSONModel([{
+                Descr  : "Fiscal year",
+                Value : ""
+            },
+			{
+				Descr  : "Personnel number",
+                Value : ""     
+            },
+			{
+				Descr  : "Current career level",
+                Value : ""      
+            },
+			{
+				Descr  : "Organizational assignment (SU)",
+                Value : ""      
+            },
+			{
+				Descr  : "Date of PEG",
+                Value : ""       
+            },
+			{
+				Descr  : "Project ID",
+                Value : projectID
+            },
+			{
+				Descr  : "Customer name",
+                Value : ""      
+            },
+			{
+				Descr  : "Name of the Project",
+                Value : ""      
+            },
+			{
+				Descr  : "Name of the Project Manager",
+                Value : ""      
+            },
+			{
+				Descr  : "Name of the Evaluator",
+                Value : ""      
+            },
+			{
+				Descr  : "Number of project days evaluated",
+                Value : ""      
+            }]);
+
+			oViewModel
+
+			this.getView().setModel(oViewModel, "DataForExport");
+
+
+			aCols = this.createColumnConfig();
+			aProducts = this.getView().getModel("DataForExport").getProperty('/');
+
+			oSettings = {
+				workbook: { columns: aCols },
+				dataSource: aProducts
+			};
+
+			oSheet = new Spreadsheet(oSettings);
+			oSheet.build()
+				.then( function() {
+					MessageToast.show('Spreadsheet export has finished');
+				})
+				.finally(oSheet.destroy);
 		}
 
 	});
--- webapp/controller/ManagerFeedback.controller.js
+++ webapp/controller/ManagerFeedback.controller.js
@@ -1,13 +1,67 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
+    "sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/export/library",
+	"sap/ui/export/Spreadsheet",
+	"sap/m/MessageToast"
 	
-], function (Controller, History) {
+], function (Controller, History, JSONModel, formatter, exportLibrary, Spreadsheet, MessageToast) {
 	"use strict";
+	var EdmType = exportLibrary.EdmType;
 	return Controller.extend("sap.ui.demo.walkthrough.controller.ManagerFeedback", {
+		formatter: formatter,
 		onInit: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("managerFeedback").attachPatternMatched(this._onObjectMatched, this);
+
+			var oViewModel = new JSONModel([{
+                Descr  : "Fiscal year",
+                Value : ""
+            },
+			{
+				Descr  : "Personnel number",
+                Value : ""     
+            },
+			{
+				Descr  : "Current career level",
+                Value : ""      
+            },
+			{
+				Descr  : "Organizational assignment (SU)",
+                Value : ""      
+            },
+			{
+				Descr  : "Date of PEG",
+                Value : ""       
+            },
+			{
+				Descr  : "Project ID",
+                Value : "{ProjectId}"      
+            },
+			{
+				Descr  : "Customer name",
+                Value : ""      
+            },
+			{
+				Descr  : "Name of the Project",
+                Value : ""      
+            },
+			{
+				Descr  : "Name of the Project Manager",
+                Value : ""      
+            },
+			{
+				Descr  : "Name of the Evaluator",
+                Value : ""      
+            },
+			{
+				Descr  : "Number of project days evaluated",
+                Value : ""      
+            }]);
+
+			this.getView().setModel(oViewModel, "DataForExport");
 		},
 
 		_onObjectMatched: function (oEvent) {
@@ -28,6 +82,40 @@ sap.ui.define([
 				var oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo("managerpeg", {}, true);
 			}
+		},
+
+		createColumnConfig: function() {
+			return [
+				{
+					label: 'Descr',
+					property: 'Descr',
+					scale: 0
+				},
+				{
+					label: 'Value',
+					property: 'Value',
+					width: '25'
+				}
+];
+		},
+
+		onExport: function() {
+			var aCols, aProducts, oSettings, oSheet;
+
+			aCols = this.createColumnConfig();
+			aProducts = this.getView().getModel("DataForExport").getProperty('/');
+
+			oSettings = {
+				workbook: { columns: aCols },
+				dataSource: aProducts
+			};
+
+			oSheet = new Spreadsheet(oSettings);
+			oSheet.build()
+				.then( function() {
+					MessageToast.show('Spreadsheet export has finished');
+				})
+				.finally(oSheet.destroy);
 		}
 
 	});
