a91beca4e0d7cee9a10e93b74465f1a97f7c1622
TaniaMohai <88478198+TaniaMohai@users.noreply.github.com>
2021-08-19 14:51:49 +0300

    reret

--- webapp/controller/ManagerFeedback.controller.js
+++ webapp/controller/ManagerFeedback.controller.js
@@ -12,11 +12,47 @@ sap.ui.define([
 
 		_onObjectMatched: function (oEvent) {
 
-		    var sPegID = oEvent.getParameter("arguments").pegID;
-			this.getView().bindElement({
-				path: "/PegReqSet(" + sPegID + ")"
-		    });
+		    // var sPegID = oEvent.getParameter("arguments").pegID;
+			// this.getView().bindElement({
+			// 	path: "/PegReqSet(" + sPegID + ")"
+		    // });
+
+
+			var sFeedbackId = oEvent.getParameter("arguments").pegID;
+
+			var oDetailPEG = this.getView().byId("Container2");
+			oDetailPEG.attachEvent("attachRequestCompleted", this.loadDetails);
+			oDetailPEG.bindElement({ path:  "/PegReqSet("+ sFeedbackId + ")" ,
+			events: {
+				dataReceived: function(oData)
+				{
+					console.log(oData);
+				}
+			}
+			});
+			
+			
+
+			  var oUserDetail = this.getView().byId("Container1");
+			  oUserDetail.bindElement({ path:  "/UserPassSet('TEST')" });
+
+			
+
+			  var sProjectId  = this.byId("GenericTileProjectId").getSubheader();
+			  var oProjectDetail = this.getView().byId("Container3");
+			  oProjectDetail.bindElement({ path: " /ProjectDetailsSet('"+ sProjectId +"')" });
+			  
+
+
 		},
+
+
+		// onAfterRendering: function()
+		// {
+		// 	this.getView().byId("Container2");
+		// },
+
+		
 
         onNavBack: function () {
 			this.navBack();
