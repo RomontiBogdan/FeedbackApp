c6b4a9e1a39791d115834af807789fb21e3ae17b
RomontiBogdan <romontibogdan@gmail.com>
2021-08-19 15:52:00 +0300

    Requests working

--- webapp/controller/ManagerFeedback.controller.js
+++ webapp/controller/ManagerFeedback.controller.js
@@ -17,33 +17,31 @@ sap.ui.define([
 			// 	path: "/PegReqSet(" + sPegID + ")"
 		    // });
 
-
+			var sProjectId  = this.byId("GenericTileProjectId").getSubheader();
 			var sFeedbackId = oEvent.getParameter("arguments").pegID;
 
 			var oDetailPEG = this.getView().byId("Container2");
-			oDetailPEG.attachEvent("attachRequestCompleted", this.loadDetails);
+		
 			oDetailPEG.bindElement({ path:  "/PegReqSet("+ sFeedbackId + ")" ,
 			events: {
 				dataReceived: function(oData)
 				{
 					console.log(oData);
-				}
+
+					var oUserDetail = this.getView().byId("Container1");
+
+					oUserDetail.bindElement({ path:  "/UserPassSet('TEST')" });
+	  
+				  
+					var oProjectDetail = this.getView().byId("Container3");
+					var sProjectId  = oData.getParameter("data").ProjectId;
+					oProjectDetail.bindElement({ path: "/ProjectDetailsSet('"+ sProjectId +"')" });
+
+				}.bind(this)
 			}
 			});
 			
-			
-
-			  var oUserDetail = this.getView().byId("Container1");
-			  oUserDetail.bindElement({ path:  "/UserPassSet('TEST')" });
-
-			
-
-			  var sProjectId  = this.byId("GenericTileProjectId").getSubheader();
-			  var oProjectDetail = this.getView().byId("Container3");
-			  oProjectDetail.bindElement({ path: " /ProjectDetailsSet('"+ sProjectId +"')" });
-			  
-
-
+		
 		},
 
 
