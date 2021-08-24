4f36eff88c7af375eb1e0b00c48ca843962b91c9
RomontiBogdan <romontibogdan@gmail.com>
2021-08-24 13:16:16 +0300

    New-Team-Feedback done

---
+++ webapp/controller/NewTeamFeedback.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "../controller/BaseController",
+    
+ 
+ ], function (BaseController) {
+    "use strict";
+    return BaseController.extend("sap.ui.demo.walkthrough.controller.NewTeamFeedback", {
+     
+       onInit: function () {
+        this.getView().byId("inputToMember").bindElement({
+            path: "/TeamManagersSet('"+ this.getCurrentUser() +"')"
+            });
+       },
+ 
+       onNavBack: function () {
+          this.navBack();
+       },
+ 
+       onUserChange: function() {
+        var SelectedItem = this.byId("inputToMember").getSelectedItem().getText();
+        this.getView().bindElement({
+           path: "/UserPassSet('" + SelectedItem + "')"
+        });
+     }
+ 
+    });
+ });
